c38de120fdf7aa491192742579d055706231ce1c
JoseAntoHL <71193848+JoseAntoHL@users.noreply.github.com>
2024-03-07 02:37:24 +0000

    Soluciones

--- Pregunta06.py
+++ Pregunta06.py
@@ -3,7 +3,7 @@ edad = int(input("Ingrese su edad: "))
 if(edad < 4):
     print("Entra gratis")
 else: 
-    if(edad >= 4 != edad <= 18):
+    if(edad >= 4 or edad <= 18):
             print("Debe pagar 5$ ")
     else:
          print("Debe pagar 10$ ")
--- Pregunta08.py
+++ Pregunta08.py
@@ -2,10 +2,25 @@ time = input("ingrese la hora(HH:MM): ")
 
 hora,minutos = time.split(":")
 
-print(hora)
+if(hora <= '24' and minutos <= '60'):
+    if(hora == '7' or hora == '8'):
+        if(hora == '8' and minutos != '00'):
+                print(" ")
+        else:
+            print("Hora de desayunar")
+    
+    if(hora == '12' or hora == '13'):
+        if(hora == '13' and minutos != '00'):
+                print(" ")
+        else:
+            print("Hora de almorzar")
+        
+    if(hora == '18' or hora == '19'):
+        if(hora == '19' and minutos != '00'):
+                print(" ")
+        else:
+            print("Hora de cenar")
 
-if(hora == 7 != hora == 8):
-    print("Hora de desayunar")
 
 
 
